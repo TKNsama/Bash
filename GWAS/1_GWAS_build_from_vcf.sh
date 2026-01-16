bfcde6c4f8a1fec394e68a84a6201e0ffe5f09bc
Kenan Tan <tan@ipk-gatersleben.de>
2026-01-16 21:45:30 +0100

    Modify SLURM script for GWAS analysis
    Updated job name and resource allocation in the SLURM script. Changed input VCF and sample file names, and modified PLINK command to allow extra chromosomes.

--- GWAS/1_GWAS_build_from_vcf.sh
+++ GWAS/1_GWAS_build_from_vcf.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=${prefix}                          # Job name
+#SBATCH --job-name=gwas1                          # Job name
 #SBATCH --auks=yes                                     # Use Kerberos
 #SBATCH --partition=cpu                                # Partition type
 #SBATCH --ntasks=1                                     # Number of tasks
-#SBATCH --cpus-per-task=32                            # Number of CPUs per task
+#SBATCH --cpus-per-task=16                            # Number of CPUs per task
 #SBATCH --mem=100G                                      # Memory allocation
 #SBATCH --nodes=1                                      # Ensure all cores are on one machine
 #SBATCH --output=job_%j.out                         # Standard output log
 #SBATCH --error=job_%j.err                          # Standard error log
-#SBATCH --exclude=slurm-03,slurm-20                    # Exclude nodes
 
 # ======= Load modules =======
 module load bcftools
@@ -17,8 +16,8 @@ module load plink
 module load gemma
 
 # ======= Configuration =======
-vcf_file="grains_248.vcf.gz"        # 输入的VCF文件
-sample_file="genotype_name.txt"     # 包含样本名的文本文件（每行一个ID）
+vcf_file="gwas.vcf.gz"        # 输入的VCF文件
+sample_file="gwas.id"     # 包含样本名的文本文件（每行一个ID）
 
 # ======= Step 1: Filter VCF by samples =======
 echo "Filtering VCF file for selected samples..."
@@ -29,7 +28,7 @@ bcftools index "$filtered_vcf"
 # ======= Step 2: Convert VCF to PLINK format =======
 echo "Converting VCF to PLINK format..."
 plink_prefix="${filtered_vcf%.vcf.gz}"  # 去掉.vcf.gz后缀
-plink --vcf "$filtered_vcf" --make-bed --double-id --out "$plink_prefix"
+plink --vcf "$filtered_vcf" --make-bed --double-id --allow-extra-chr --out "$plink_prefix"
 
 # ======= Step 3: PCA Analysis =======
 echo "Running PCA..."
